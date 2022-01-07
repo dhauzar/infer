93d4043441471cfd49382b3332d9ab5066791418
David Hauzar <hauzar@adacore.com>
2022-01-07 07:21:51 +0100

    Revert "[I] T221-007 Prune also stack variables."
    This reverts commit 1d6b4977b5c64b752162e3aa25bef132de1811da.

--- infer/src/bufferoverrun/bufferOverrunSemantics.ml
+++ infer/src/bufferoverrun/bufferOverrunSemantics.ml
@@ -611,29 +611,26 @@ module Prune = struct
       tgts acc
 
 
-  let prune_stack_var pruned_id prune ({mem} as astate) =
-    let pruned_loc = Loc.of_id pruned_id in
-    let pruned_val = Mem.find pruned_loc mem in
-    let resulting_val = prune pruned_val in
-    let mem = Mem.update_mem (PowLoc.singleton pruned_loc) resulting_val mem in
-    (pruned_val, resulting_val, {astate with mem})
-
-
   let prune_unop : Exp.t -> t -> t =
    fun e ({mem} as astate) ->
     match e with
     | Exp.Var x ->
-        (* (1) prune the stack variable corresponding to x *)
-        let v, v', astate = prune_stack_var x Val.prune_ne_zero astate in
-        (* (2) prune heap variables which are aliases of the stack variable *)
         let accum_prune_var rhs tgt acc =
           match tgt with
-          | AliasTarget.Simple {i} when IntLit.iszero i && not (Val.is_bot v) ->
-              update_mem_in_prune rhs v' acc |> prune_linked_list_index rhs mem
+          | AliasTarget.Simple {i} when IntLit.iszero i ->
+              (let v = Mem.find rhs mem in
+               if Val.is_bot v then acc
+               else
+                 let v' = Val.prune_ne_zero v in
+                 update_mem_in_prune rhs v' acc )
+              |> prune_linked_list_index rhs mem
               |> prune_iterator_offset_objc rhs mem
-          | AliasTarget.Empty when not (Val.is_bot v) ->
-              let v' = Val.prune_length_eq_zero v in
-              update_mem_in_prune rhs v' acc
+          | AliasTarget.Empty ->
+              let v = Mem.find rhs mem in
+              if Val.is_bot v then acc
+              else
+                let v' = Val.prune_length_eq_zero v in
+                update_mem_in_prune rhs v' acc
           | AliasTarget.Fgets ->
               let strlen_loc = Loc.of_c_strlen rhs in
               let v = Mem.find strlen_loc mem in
@@ -648,13 +645,14 @@ module Prune = struct
         in
         AliasTargets.fold accum_prune_var (Mem.find_alias_id x mem) astate
     | Exp.UnOp (Unop.LNot, Exp.Var x, _) ->
-        (* (1) prune the stack variable corresponding to x *)
-        let v, v', astate = prune_stack_var x Val.prune_eq_zero astate in
-        (* (2) prune heap variables which are aliases of the stack variable *)
         let accum_prune_not_var rhs tgt acc =
           match tgt with
-          | AliasTarget.Simple {i} when IntLit.iszero i && not (Val.is_bot v) ->
-              update_mem_in_prune rhs v' acc
+          | AliasTarget.Simple {i} when IntLit.iszero i ->
+              let v = Mem.find rhs mem in
+              if Val.is_bot v then acc
+              else
+                let v' = Val.prune_eq_zero v in
+                update_mem_in_prune rhs v' acc
           | AliasTarget.Empty ->
               let v = Mem.find rhs mem in
               if Val.is_bot v then acc
@@ -709,14 +707,12 @@ module Prune = struct
   let prune_simple_alias =
     let prune_alias_core ~val_prune_eq ~val_prune_le:_ ~make_pruning_exp _location
         integer_type_widths x e ({mem} as astate) =
-      let expr_val = eval integer_type_widths e mem in
-      (* (1) prune the stack variable corresponding to x *)
-      let lhs, _, astate =
-        prune_stack_var x (fun loc_val -> val_prune_eq loc_val expr_val) astate
-      in
-      (* (2) prune the heap variable which is an simple alias of the stack variable *)
       List.fold (Mem.find_simple_alias x mem) ~init:astate ~f:(fun acc (lv, i) ->
-          let rhs = if IntLit.iszero i then expr_val else Val.minus_a expr_val (Val.of_int_lit i) in
+          let lhs = Mem.find lv mem in
+          let rhs =
+            let v' = eval integer_type_widths e mem in
+            if IntLit.iszero i then v' else Val.minus_a v' (Val.of_int_lit i)
+          in
           if Val.is_bot lhs || Val.is_bot rhs then acc
           else
             let v = val_prune_eq lhs rhs in
